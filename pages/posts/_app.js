4c8bbe29338d15da14e6b02925b6b9624f909d98
Fields Marshall <fields.marshall@gmail.com>
2023-09-21 11:06:14 -0700

    removed item

--- pages/posts/_app.js
+++
@@ -1,6 +0,0 @@
-// These styles apply to every route in the application
-import "./globals.css";
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
