b6eda6818f6c3caeae542b822c25e1ed475f0bfb
Fields Marshall <fields.marshall@gmail.com>
2024-10-31 01:35:21 -0700

    markdown processing

--- components/MarkdownContent.js
+++ components/MarkdownContent.js
@@ -1,12 +1,10 @@
 // components/MarkdownContent.js
 import React from "react";
-import ReactMarkdown from "react-markdown";
-
 import { TinaMarkdown } from "tinacms/dist/rich-text";
 
 export default function MarkdownContent({ markdownText }) {
   return (
-    <article className="prose text-left  dark:prose-invert  px-3 mx-3">
+    <article className="prose prose-lg text-left  dark:prose-invert  px-3 mx-3">
       <TinaMarkdown content={markdownText} />
     </article>
   );
--- components/blocks/ContentBlock.js
+++ components/blocks/ContentBlock.js
@@ -1,9 +1,10 @@
 import { TinaMarkdown } from "tinacms/dist/rich-text";
+import MarkdownContent from "../MarkdownContent";
 
 export const ContentBlock = ({ data }) => {
   return (
     <div className="max-w-4xl mx-auto text-lg my-3 text-center">
-      {data.textz && <TinaMarkdown content={data.textz} />}
+      {data.textz && <MarkdownContent markdownText={data.textz} />}
     </div>
   );
 };
